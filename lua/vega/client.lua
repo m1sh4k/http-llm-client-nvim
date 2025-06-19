91881639a8f4a1ed0b8c5065de3885183a48ee3f
m1sh4k <mishaivan373@gmail.com>
2025-06-19 19:13:41 +0300

    switched_to_vim.uv_insteadof_lua-socket

--- lua/vega/client.lua
+++ lua/vega/client.lua
@@ -1,32 +1,47 @@
 local M = {}
 
-
-
-function M.create_completion(args)
-    local host, port = "127.0.0.1", 9999  -- changed 'local host' instead of 'local localhost'
-    local socket = require("socket")
-    local tcp = assert(socket.tcp())
+function M.CreateCompletion(args)
     local pattern_code = "```.-```"
-    local to_insert = ""
-
-    tcp:connect(host, port)
-    tcp:send(args)
---    tcp:send("write python function that takes two int as arguments and retuens наименьший общий делитель, write only code, no comments or notes at all and write another function that returns sum of arguments")
-
--- changed receive("*a") to receive("*l") to avoid hanging
-    local data, err, partial = tcp:receive("*a")
-
---print(data)
---print('---------------')
-
-    for i in string.gmatch(data, pattern_code, 1) do
-        to_insert = to_insert .. "\n" .. i
+    local to_insert = ''
+    local client = vim.uv.new_tcp()
+    if not client then
+        error("Failed to create TCP client handle")
+        return
     end
-    to_insert = to_insert:gsub("```.-\n", "")
-    to_insert = to_insert:gsub("```$", "")
---print(string.sub(data, string.find(data, pattern_code, 1))) --type: ignore
-    print(to_insert)
-    tcp:close()  -- close the socket after use
+    local host, port = "127.0.0.1", 9999
+--   
+    vim.uv.tcp_connect(client, host, port, function (error)
+        if error then
+            print("Connection error:", error)
+            vim.uv.close(client)
+            return
+        end
+    end)
+    vim.uv.write(client, args, function (sending_error)
+        if sending_error then
+            print('Sending error:', sending_error)
+            vim.uv.close(client)
+            return
+        end
+    end)
+    vim.uv.read_start(client, function (read_error, response)
+        if read_error then
+            print('Read error: ', read_error)
+            vim.uv.close(client)
+            return
+        end
+        if response then
+            for i in string.gmatch(response, pattern_code, 1) do
+                to_insert = to_insert .. "\n" .. i
+            end
+            to_insert = to_insert:gsub("```.-\n", "")
+            to_insert = to_insert:gsub("```$", "")
+            print(to_insert)
+       else
+            vim.uv.close(client)
+        end
+
+   end)
 end
 
 return M
--- lua/vega/init.lua
+++ lua/vega/init.lua
@@ -3,7 +3,7 @@ local M = {}
 local client = require('vega.client')
 function M.setup()
   vim.api.nvim_create_user_command('CreateCompletion', function(opts)
-    client.create_completion(opts.args)
+    client.CreateCompletion(opts.args)
   end, {})
 end
 
